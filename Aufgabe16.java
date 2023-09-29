d8a8609e8e53f34dab9a8a802be043d8e2c00ab9
Robert <r.kohl@xentego.at>
2023-09-29 18:51:54 +0200

    Aufgabe 16 mit Modulo gelöst

--- Aufgabe16.java
+++ Aufgabe16.java
@@ -1,31 +1,64 @@
+import com.sun.jdi.event.MethodEntryEvent;
+
 public class Aufgabe16 {
     public static void main(String[] args) {
 
         int asciiStart = 65;
         int asciiEnd = 90;
         int quantityAscii = asciiEnd - asciiStart + 1;
-        int howMuch = 5;
-        boolean condition = true;
+        int howMuch =-2;
+     //   boolean condition = true;
         char[] allBigLetters = new char[quantityAscii];
-        char[] lettersShift = new char[quantityAscii];
-        //  int i = 0;
 
         for (int i = 0; i < allBigLetters.length; i++) {
             allBigLetters[i] = (char) (asciiStart + i);
-
         }
         System.out.println(allBigLetters);
-        int j = 0;
-        for (int i = 0; i < lettersShift.length; i++) {
-            if (i < (lettersShift.length - howMuch)) {
-                lettersShift[i] = allBigLetters[howMuch + i];
-            } else {
-                lettersShift[i] = allBigLetters[j];
-                j++;
-            }
-        }
-        System.out.println(lettersShift);
 
+        char[] lettersShift = new char[quantityAscii];
+        shiftLeftRight(howMuch, lettersShift, quantityAscii, asciiStart);
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//        int asciiStart = 65;
+//        int asciiEnd = 90;
+//        int quantityAscii = asciiEnd - asciiStart + 1;
+//        int howMuch = 0;
+//        boolean condition = true;
+//        char[] allBigLetters = new char[quantityAscii];
+//        char[] lettersShift = new char[quantityAscii];
+//        //  int i = 0;
+//
+//        for (int i = 0; i < allBigLetters.length; i++) {
+//            allBigLetters[i] = (char) (asciiStart + i);
+//        }
+//        System.out.println(allBigLetters);
+//        int j = 0;
+//        if (howMuch >= 0) { // left shifting
+//            for (int i = 0; i < lettersShift.length; i++) {
+//                if (i < (lettersShift.length - howMuch)) {
+//                    lettersShift[i] = allBigLetters[howMuch + i];
+//                } else {
+//                    lettersShift[i] = allBigLetters[j];
+//                    j++;
+//                }
+//            }
+//        } else {    // right shifting
+//            j = 0;
+//            for (int i = 0; i < lettersShift.length; i++) {
+//                if (i < (-howMuch)) {
+//                    lettersShift[i] = allBigLetters[lettersShift.length + howMuch + i];
+//                } else {
+//                    lettersShift[i] = allBigLetters[j];
+//                    j++;
+//                }
+//            }
+//        }
+//        System.out.println(lettersShift);
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
 //        while (condition) {
 //            allBigLetters[i] = (char) (asciiStart + howMuch + i);
 //            if (i > (quantityAscii - howMuch - 2)) {
@@ -50,5 +83,31 @@ public class Aufgabe16 {
 //        }
 //        System.out.println(allBigLetters);
 
+    }
+    ///////////////////////////////////////////////////
+    //METHODEN
+
+    static void shiftLeft(int howMuch, char[] lettersShift, int quantityAscii, int asciiStart) {
+        for (int i = 0; i < lettersShift.length; i++) {
+            // Left shifting
+            lettersShift[i] = (char) (asciiStart + (howMuch + i) % quantityAscii);
+        }
+        System.out.println(lettersShift);
+    }
+
+    static void shiftRight(int howMuch, char[] lettersShift,int quantityAscii, int asciiStart) {
+        for (int i = 0; i < lettersShift.length; i++) {
+            // right shift
+            lettersShift[i] = (char) (asciiStart + (quantityAscii + howMuch + i) % quantityAscii);
+        }
+        System.out.println(lettersShift);
+    }
+
+    static void shiftLeftRight (int howMuch, char[] lettersShift, int quantityAscii, int asciiStart){
+        if (howMuch >= 0) {
+            shiftLeft(howMuch, lettersShift, quantityAscii, asciiStart);
+        } else {
+            shiftRight(howMuch, lettersShift, quantityAscii, asciiStart);
+        }
     }
 }
